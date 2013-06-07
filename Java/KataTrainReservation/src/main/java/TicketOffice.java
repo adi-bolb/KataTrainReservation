844f91d40a4d0e82d7d656971d0a673e46177cbc
Adi Bolboaca <adi_bolb123@yahoo.com>
2013-06-07 16:44:08 +0300

    Added gitignore

---
+++ Java/KataTrainReservation/src/main/java/TicketOffice.java
@@ -0,0 +1,14 @@
+package main.java;
+
+public class TicketOffice {
+    
+    public TicketOffice(String trainDataService, String bookingReferenceService) {
+		//TODO: implement this code!
+    }
+
+    public Reservation makeReservation(ReservationRequest request) {
+		//TODO: implement this code!
+		return null;
+    }
+
+}
